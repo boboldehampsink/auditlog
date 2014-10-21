da569d7a023510bf630d525f08dba4eb15b2e611
Tim Kelty <tim@fusionary.com>
2014-10-21 15:20:12 -0400

    Use getContent to parse field data

--- services/AuditLogService.php
+++ services/AuditLogService.php
@@ -1,12 +1,12 @@
 <?php
 namespace Craft;
 
-class AuditLogService extends BaseApplicationComponent 
+class AuditLogService extends BaseApplicationComponent
 {
 
     public function log($criteria)
     {
-    
+
         // Build specific criteria
         $condition = '';
         $params = array();
@@ -23,7 +23,7 @@ class AuditLogService extends BaseApplicationComponent
             $params[':search'] = '%' . addcslashes($criteria->search, '%_') . '%';
         }
         $condition = substr($condition, 0, -5);
-    
+
         // Get logs from record
         return AuditLogModel::populateModels(AuditLogRecord::model()->findAll(array(
             'order'     => 'id desc',
@@ -32,22 +32,22 @@ class AuditLogService extends BaseApplicationComponent
             'condition' => $condition,
             'params'    => $params
         )));
-    
+
     }
-    
+
     public function view($id)
     {
-    
+
         // Get log from record
         $log = AuditLogModel::populateModel(AuditLogRecord::model()->findByPk($id));
-        
+
         // Create diff
         $diff = array();
-                
+
         // Loop through content
-        foreach($log->after as $handle => $item) 
+        foreach($log->after as $handle => $item)
         {
-                                
+
             // Set parsed values
             $diff[$handle] = array(
                 'label'   => $item['label'],
@@ -55,78 +55,78 @@ class AuditLogService extends BaseApplicationComponent
                 'after'   => $item['value'],
                 'before'  => $log['before'][$handle]['value']
             );
-        
+
         }
-        
+
         // Set diff
         $log->setAttribute('diff', $diff);
-        
+
         // Return the log
         return $log;
-    
+
     }
-    
+
     // Parse field values
-    public function parseFieldData($handle, $data) 
+    public function parseFieldData($handle, $data)
     {
-    
+
         // Do we have any data at all
         if(!is_null($data)) {
-        
+
             // Get field info
             $field = craft()->fields->getFieldByHandle($handle);
-            
+
             // If it's a field ofcourse
             if(!is_null($field)) {
-            
+
                 // For some fieldtypes the're special rules
                 switch($field->type) {
-                
+
                     case AuditLogModel::FieldTypeEntries:
                     case AuditLogModel::FieldTypeCategories:
                     case AuditLogModel::FieldTypeAssets:
                     case AuditLogModel::FieldTypeUsers:
-                        
+
                         // Show names
-                        $data = implode(', ', $data->find());
-                        
+                        $data = implode(', ', is_array($data) ? $data : []);
+
                         break;
-                    
+
                     case AuditLogModel::FieldTypeLightswitch:
-                    
+
                         // Make data human readable
                         switch($data) {
-                        
+
                             case "0":
                                 $data = Craft::t("No");
                                 break;
-                            
+
                             case "1":
                                 $data = Craft::t("Yes");
                                 break;
-                        
+
                         }
-                        
+
                         break;
-                
+
                 }
-            
+
             }
-        
+
         } else {
-        
+
             // Don't return null, return empty
             $data = "";
-        
+
         }
-        
+
         // If it's an array, make it a string
         if(is_array($data)) {
             $data = StringHelper::arrayToString($data);
         }
-        
+
         return $data;
-    
+
     }
-    
+
 }
