3640a76060abb67f8a58daed6868b139bbe5c41a
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-12-04 08:45:01 +0100

    Updated Unit Test setup

--- tests/AuditLogTest.php
+++ tests/AuditLogTest.php
@@ -7,26 +7,9 @@ class AuditLogTest extends BaseTest
     public function setUp()
     {
     
-        // PHPUnit complains about not settings this
-        date_default_timezone_set('UTC');
-    
-        // Get dependencies
-        $dir = __DIR__;
-        $map = array(
-            '\\Craft\\AuditLogModel'   => '/../models/AuditLogModel.php',
-            '\\Craft\\AuditLogRecord'  => '/../records/AuditLogRecord.php',
-            '\\Craft\\AuditLogService' => '/../services/AuditLogService.php'
-        );
-
-        // Inject them
-        foreach($map as $classPath => $filePath) {
-            if(!class_exists($classPath, false)) {
-                require_once($dir . $filePath);
-            }
-        }
-    
-        // Set components we're going to use
-        $this->setComponent(craft(), 'auditLog', new AuditLogService);
+        // Load plugins
+        $pluginsService = craft()->getComponent('plugins');
+        $pluginsService->loadPlugins();
     
     } 
     
